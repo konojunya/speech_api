165c8136e22d3743598c169201a1225e1e8e68e0
konojunya <junya03dance@gmail.com>
2016-10-25 16:42:12 +0900

    2016年 10月25日 火曜日 16時42分12秒 JST

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 var express = require("express");
-var request = require('superagent');
+var request = require('request');
 var PORT = 4000 || process.env.PORT;
 var app = express();
+
+var status = {
+	nickname: "じゅんじゅん",
+	nickname_y: "ジュンジュン",
+	sex: "男",
+	bloodtype: "O",
+	birthdateY: 1997,
+	birthdateM: 3,
+	birthdateD: 3,
+	age: 19,
+	constellations: "魚座",
+	place: "大阪"
+};
 
 app.get("/",function(req,res){
 	res.sendfile("index.html");
 })
 app.get("/api",function(req,res){
-	var value = req.query.text;
-	value = encodeURIComponent(value)
-	var url = "https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?utt="+value+"&APIKEY="+process.env.DOCOMO_API_KEY;
-	request
-		.get(url)
-		.end(function(err,r){
-			console.log(r.body)
-			res.json({
-				res: r.body.utt
-			})
+	var value = req.query.text,
+			url = "https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=",
+			token = process.env.DOCOMO_API_KEY;
+
+	status.utt = value;
+	
+	var param = {
+		body: JSON.stringify(status),
+		"Content-Type": "application/json"
+	}
+
+	request.post(url+token,param,function(err,response,data){
+		if (err) throw err;
+		var body = JSON.parse(data)
+		status.context = body.context;
+		body.utt = value == "バイバイ" ? "またねー！" : body.utt
+		res.json({
+			res: body.utt
 		})
+	});
+
 })
 
 app.listen(PORT,function(){
